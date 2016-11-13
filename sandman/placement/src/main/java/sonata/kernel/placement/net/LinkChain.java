42a1160d51ffd1e404a877ac716bc325571e2456
Alex <foo@example.org>
2016-11-13 02:59:03 +0100

    Fix chaining functions

--- sandman/placement/src/main/java/sonata/kernel/placement/net/LinkChain.java
+++ sandman/placement/src/main/java/sonata/kernel/placement/net/LinkChain.java
@@ -4,8 +4,12 @@ import sonata.kernel.placement.config.PopResource;
 
 public class LinkChain {
 
-    public PopResource popResource;
-    public String stackName;
+    // Datacenter names
+    public PopResource srcDc;
+    public PopResource dstDc;
+
+    public String srcStack;
+    public String dstStack;
 
     // Names defined by Translator
     public String srcServer;
@@ -17,49 +21,17 @@ public class LinkChain {
     public String srcNode;
     public String srcInterface;
     public String dstNode;
-    public String dstInterface;
 
-    public LinkChain(PopResource popResource, String stackName, String srcServer, String srcPort, String dstServer, String dstPort){
-        this.popResource = popResource;
-        this.stackName = stackName;
+    public LinkChain(PopResource srcDc, String srcStack, String srcServer, String srcPort,
+                     PopResource dstDc, String dstStack, String dstServer, String dstPort) {
+        this.srcDc = srcDc;
+        this.dstDc = dstDc;
+        this.srcStack = srcStack;
+        this.dstStack = dstStack;
         this.srcServer = srcServer;
         this.srcPort = srcPort;
         this.dstServer = dstServer;
         this.dstPort = dstPort;
-        this.srcNode = convertNodeName(this.srcServer);
-        this.dstNode = convertNodeName(this.dstServer);
-        this.srcInterface = convertInterfaceName(this.srcPort);
-        this.dstInterface = convertInterfaceName(this.dstPort);
+
     }
-
-    /*
-     * ! Assume that serverName is unique in the stack!!!
-     */
-    protected String convertNodeName(String serverName){
-        String nodeName;
-
-        nodeName = serverName.split(":")[0];
-
-        nodeName = nodeName.substring(0,12).replace("-","_");
-
-        nodeName = popResource.getPopName() + "_" + this.stackName + "_" + nodeName;
-
-        return nodeName;
-    }
-
-    protected String convertInterfaceName(String portName){
-        String interfaceName;
-        String[] portNameSplitted = portName.split(":");
-        interfaceName = portNameSplitted[0] + "-" + portNameSplitted[1] + "-";
-
-        if (portNameSplitted[2].equals("input") || portNameSplitted[2].equals("in"))
-            interfaceName += "in";
-        else if(portNameSplitted[2].equals("output") || portNameSplitted[2].equals("out"))
-            interfaceName += "out";
-        else
-            interfaceName += portNameSplitted[2];
-
-        return interfaceName;
-    }
-
 }

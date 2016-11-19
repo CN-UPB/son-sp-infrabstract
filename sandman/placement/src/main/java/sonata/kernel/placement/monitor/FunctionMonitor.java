23f03c61bcbae3479e232472268afd6be9d5ca18
Alex <foo@example.org>
2016-11-19 23:00:01 +0100

    Fix monitoring + Add Monitor test with single vnf

--- sandman/placement/src/main/java/sonata/kernel/placement/monitor/FunctionMonitor.java
+++ sandman/placement/src/main/java/sonata/kernel/placement/monitor/FunctionMonitor.java
@@ -5,10 +5,11 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.apache.commons.io.IOUtils;
-import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.log4j.Logger;
 import sonata.kernel.VimAdaptor.commons.vnfd.Unit;
 import sonata.kernel.VimAdaptor.commons.vnfd.UnitDeserializer;
@@ -16,11 +17,12 @@ import sonata.kernel.placement.config.PopResource;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.concurrent.Future;
 
 /**
- * Controls a monitor thread to monitor one function
+ * Requests and saves monitor information for one function
  */
-public class FunctionMonitor {
+public class FunctionMonitor implements FutureCallback<HttpResponse> {
 
     final static Logger logger = Logger.getLogger(FunctionMonitor.class);
 
@@ -29,130 +31,116 @@ public class FunctionMonitor {
     public String function;
     public String requestPath;
 
-    public long interval; // in milliseconds
-    public Thread monitorThread;
-    public MonitorInstance monitorInstance;
     public CloseableHttpClient httpClient = null;
     public HttpGet httpRequest;
 
     public int historySize = 100;
-    // resources: cpu in s, memory in bytes, network io?, disk io?
-    // TODO: cumulative values?
-    // historySize x {system time in nanoseconds, value}
-    ValueHistory cpuHistory;
-    ValueHistory memoryHistory;
+    ValueHistoryDouble cpuHistory;
+    ValueHistoryLong memoryHistory;
 
-    public FunctionMonitor(PopResource datacenter, String stack, String function, long interval){
+    public FunctionMonitor(PopResource datacenter, String stack, String function){
         this.dc = datacenter;
         this.stack = stack;
         this.function = function;
-        this.interval = interval;
-        this.monitorInstance = new MonitorInstance();
-        this.monitorThread = new Thread(this.monitorInstance);
-        this.requestPath = datacenter.getMonitoringEndpoint()+"v1/monitor/"+function;
+        //this.requestPath = datacenter.getMonitoringEndpoint()+"v1/monitor/"+function;
+        this.requestPath = datacenter.getMonitoringEndpoint()+"v1/monitor/"+dc.getPopName()+"/"+stack+"/"+function;
         httpRequest = new HttpGet(this.requestPath);
-        cpuHistory = new ValueHistory(historySize);
-        memoryHistory = new ValueHistory(historySize);
+        cpuHistory = new ValueHistoryDouble(historySize);
+        memoryHistory = new ValueHistoryLong(historySize);
     }
 
-    public void startMonitor(){
-        this.monitorThread.start();
+    public void requestMonitorStats(CloseableHttpAsyncClient asyncClient){
+        logger.debug("Requested monitor status for "+dc.getPopName()+"_"+stack+"_"+function);
+        Future<HttpResponse> f = asyncClient.execute(this.httpRequest, this);
     }
 
-    public void stopMonitor(){
-        this.monitorThread.interrupt();
-    }
+    @Override
+    public void completed(HttpResponse response) {
 
-    class MonitorInstance implements Runnable{
-
-        public boolean stopMonitoring = false;
-
-        public void run(){
-
-            while(true) {
-
-                // Sleep until next status request
-                try {
-                    Thread.sleep(interval);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                if(stopMonitoring)
-                    return;
-
-                if(httpClient == null)
-                    httpClient = HttpClients.createDefault();
-
-                CloseableHttpResponse response = null;
-                try {
-                    response = httpClient.execute(httpRequest);
-                    String json = IOUtils.toString(response.getEntity().getContent(), "utf-8");
-                    readJsonMonitoring(json);
-
-                    cpuHistory.addValue(0,0);
-                    memoryHistory.addValue(0,0);
-
-                    // TODO: Create monitor message if necessary
-                    // TODO: Maybe check some limits
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    // TODO: Add fallback values?
-                }
-            }
+        String json = null;
+        MonitorStats stats = null;
+        try {
+            json = IOUtils.toString(response.getEntity().getContent(), "utf-8");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        stats = readJsonMonitoring(json);
+
+        if(stats != null) {
+            cpuHistory.addValue(stats.getSysTime(),stats.getCpu());
+            memoryHistory.addValue(stats.getSysTime(), stats.getMemoryUsed());
+        }
+
+        // TODO: Create monitor message if necessary
+        // TODO: Maybe check some limits
+
+        logger.debug("Incoming monitor status for "+dc.getPopName()+"_"+stack+"_"+function+": "+new String(json));
     }
 
-    public Object readJsonMonitoring(String text){
+    @Override
+    public void failed(Exception e) {
+        logger.debug("Monitor request failed for "+dc.getPopName()+"_"+stack+"_"+function);
+    }
+
+    @Override
+    public void cancelled() {
+        logger.debug("Monitor request cancelled for "+dc.getPopName()+"_"+stack+"_"+function);
+    }
+
+    public MonitorStats readJsonMonitoring(String text){
         ObjectMapper mapper = new ObjectMapper(new JsonFactory());
         SimpleModule module = new SimpleModule();
 
         module.addDeserializer(Unit.class, new UnitDeserializer());
         mapper.registerModule(module);
         mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
-        Object json = null;
+        MonitorStats stats = null;
         try {
-            // TODO: Add Monitoring data structure
-            json = mapper.readValue(text, Object.class);
+            stats = mapper.readValue(text, MonitorStats.class);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.debug("MonitorStats JSON parsing failure",e);
         }
-        return json;
+        return stats;
     }
 
     /**
      * Ringbuffer like array structure
      */
-    public static class ValueHistory{
+    public static class ValueHistoryLong{
 
-        public long[][] data;
+        public long[] time;
+        public long[] data;
         public int nextIndex = 0;
 
-        public ValueHistory(int length){
-            data = new long[length][2];
+        public ValueHistoryLong(int length){
+            data = new long[length];
+            time = new long[length];
         }
 
         public void addValue(long time, long value) {
-            data[nextIndex][0] = time;
-            data[nextIndex][1] = value;
+            this.time[nextIndex] = time;
+            this.data[nextIndex] = value;
             nextIndex++;
             if(nextIndex>=data.length)
                 nextIndex = 0;
         }
 
-        public long[][] getLast(int number){
+        public int getLast(int number, long[] time, long[] data){
             int newLength = Math.min(number, data.length);
             if(nextIndex >= newLength) {
                 // copy in one go
-                return Arrays.copyOfRange(data, nextIndex-newLength, nextIndex);
+                System.arraycopy(this.data, newLength-data.length, data, 0, newLength);
+                System.arraycopy(this.time, newLength-data.length, time, 0, newLength);
             } else {
-                long[][] newData = new long[newLength][2];
                 // two copies since range is split
-                System.arraycopy(data, 0, newData, newLength-data.length, nextIndex);
-                System.arraycopy(data, data.length-(newLength-nextIndex), newData, 0, newLength-nextIndex);
-                return newData;
+                System.arraycopy(this.data, 0, data, newLength-nextIndex, nextIndex);
+                System.arraycopy(this.time, 0, time, newLength-nextIndex, nextIndex);
+
+                System.arraycopy(this.data, this.data.length-(newLength-nextIndex), data, 0, newLength-nextIndex);
+                System.arraycopy(this.time, this.time.length-(newLength-nextIndex), time, 0, newLength-nextIndex);
             }
+            return newLength;
         }
 
         public int length(){
@@ -160,4 +148,44 @@ public class FunctionMonitor {
         }
     }
 
+    public static class ValueHistoryDouble{
+
+        public long[] time;
+        public double[] data;
+        public int nextIndex = 0;
+
+        public ValueHistoryDouble(int length){
+            time = new long[length];
+            data = new double[length];
+        }
+
+        public void addValue(long time, double value) {
+            this.time[nextIndex] = time;
+            this.data[nextIndex] = value;
+            nextIndex++;
+            if(nextIndex>=data.length)
+                nextIndex = 0;
+        }
+
+        public int getLast(int number, long[] time, double[] data){
+            int newLength = Math.min(number, data.length);
+            if(nextIndex >= newLength) {
+                // copy in one go
+                System.arraycopy(this.data, newLength-data.length, data, 0, newLength);
+                System.arraycopy(this.time, newLength-data.length, time, 0, newLength);
+            } else {
+                // two copies since range is split
+                System.arraycopy(this.data, 0, data, newLength-nextIndex, nextIndex);
+                System.arraycopy(this.time, 0, time, newLength-nextIndex, nextIndex);
+
+                System.arraycopy(this.data, this.data.length-(newLength-nextIndex), data, 0, newLength-nextIndex);
+                System.arraycopy(this.time, this.time.length-(newLength-nextIndex), time, 0, newLength-nextIndex);
+            }
+            return newLength;
+        }
+
+        public int length(){
+            return data.length;
+        }
+    }
 }
--- sandman/placement/src/main/java/sonata/kernel/placement/monitor/MonitorStats.java
+++ sandman/placement/src/main/java/sonata/kernel/placement/monitor/MonitorStats.java
@@ -4,88 +4,111 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class MonitorStats {
 
-    /* current
-    {"NET_out": "648 B", "MEM_used": "1.07 MiB", "PIDS": "3", "NET_in": "1.596 kB",
-    "MEM_limit": "15.67 GiB", "CPU": "0.00%", "MEM_%": "0.01%"}
-    */
-    /* experimental
-    {'NET_out': [], 'CPU_%': 0.0, 'MEM_used': 1101824, 'PIDS': 4,
-    'NET_in': ['Total', '0'], 'SYS_time': 1479224676000000000L, 'MEM_limit': 16827117568, 'MEM_%': 6.54790694572272e-05}
-    */
-    /* new
+    /* example
     {'MEM_used': 790528, 'PIDS': 2, 'BLOCK_out': [], 'NET_out': 648,
      'SYS_time': 1479522691737230080L, 'MEM_limit': 16827117568,
      'NET_in': 648, 'BLOCK_in': ['Total', '0'], 'MEM_%': 4.6979406710947397e-05, 'CPU_%': 0.0}
     */
 
+    // Network traffic of all network interfaces within the controller
     @JsonProperty("NET_out")
-    String netOut;
+    long netOut;
     @JsonProperty("NET_in")
-    String netIn;
-    @JsonProperty("MEM_used")
-    String memoryUsed;
-    @JsonProperty("PIDS")
-    String processIds;
-    @JsonProperty("MEM_limit")
-    String memoryLimit;
-    @JsonProperty("CPU")
-    String cpu;
+    long netIn;
+    @JsonProperty("BLOCK_out") // Disk - write in Bytes
+    String[] blockOut;
+    @JsonProperty("BLOCK_in") // Disk - read in Bytes
+    String[] blockIn;
+    @JsonProperty("MEM_used") // Bytes of memory used from the docker container
+    long memoryUsed;
+    @JsonProperty("MEM_limit") // Bytes of memory the docker container could use
+    long memoryLimit;
     @JsonProperty("MEM_%")
-    String memoryPercentage;
+    float memoryPercentage;
+    @JsonProperty("PIDS") // Number of processes
+    int processes;
+    @JsonProperty("CPU_%")
+    double cpu;
+    @JsonProperty("SYS_time")
+    long sysTime;
 
-    public String getNetOut() {
+    public long getNetOut() {
         return netOut;
     }
 
-    public void setNetOut(String netOut) {
+    public void setNetOut(long netOut) {
         this.netOut = netOut;
     }
 
-    public String getNetIn() {
+    public long getNetIn() {
         return netIn;
     }
 
-    public void setNetIn(String netIn) {
+    public void setNetIn(long netIn) {
         this.netIn = netIn;
     }
 
-    public String getMemoryUsed() {
+    public String[] getBlockOut() {
+        return blockOut;
+    }
+
+    public void setBlockOut(String[] blockOut) {
+        this.blockOut = blockOut;
+    }
+
+    public String[] getBlockIn() {
+        return blockIn;
+    }
+
+    public void setBlockIn(String[] blockIn) {
+        this.blockIn = blockIn;
+    }
+
+    public long getMemoryUsed() {
         return memoryUsed;
     }
 
-    public void setMemoryUsed(String memoryUsed) {
+    public void setMemoryUsed(long memoryUsed) {
         this.memoryUsed = memoryUsed;
     }
 
-    public String getProcessIds() {
-        return processIds;
-    }
-
-    public void setProcessIds(String processIds) {
-        this.processIds = processIds;
-    }
-
-    public String getMemoryLimit() {
+    public long getMemoryLimit() {
         return memoryLimit;
     }
 
-    public void setMemoryLimit(String memoryLimit) {
+    public void setMemoryLimit(long memoryLimit) {
         this.memoryLimit = memoryLimit;
     }
 
-    public String getCpu() {
-        return cpu;
-    }
-
-    public void setCpu(String cpu) {
-        this.cpu = cpu;
-    }
-
-    public String getMemoryPercentage() {
+    public float getMemoryPercentage() {
         return memoryPercentage;
     }
 
-    public void setMemoryPercentage(String memoryPercentage) {
+    public void setMemoryPercentage(float memoryPercentage) {
         this.memoryPercentage = memoryPercentage;
+    }
+
+    public int getProcesses() {
+        return processes;
+    }
+
+    public void setProcesses(int processes) {
+        this.processes = processes;
+    }
+
+    public double getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(double cpu) {
+        this.cpu = cpu;
+    }
+
+    public long getSysTime() {
+        return sysTime;
+    }
+
+    public void setSysTime(long sysTime) {
+        this.sysTime = sysTime;
     }
 }

f357e5884ffcd2eabaacfb795d3514e990d2cc30
Alex <foo@example.org>
2017-02-14 23:00:58 +0100

    Add SubnetUtilsV6 documentation

--- sandman/placement/src/main/java/sonata/kernel/placement/service/SubnetUtilsV6.java
+++ sandman/placement/src/main/java/sonata/kernel/placement/service/SubnetUtilsV6.java
@@ -1,29 +1,57 @@
 package sonata.kernel.placement.service;
 
-
 import org.apache.commons.net.util.SubnetUtils;
-
-import java.math.BigInteger;
-import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+/**
+ * Handles IPv6 Subnets and IP addresses.
+ * Inspired by the SubnetUtils class from the org.apache.commons.net.
+ * Not used since the emulator configures the network resources.
+ */
 public class SubnetUtilsV6 {
 
+    /**
+     * Utils for IPv4 Addresses used to handle IPv4 subnets.
+     */
     SubnetUtils oldUtils;
 
+    /**
+     * Number of bits in the IPv6 address
+     */
     private static final int NBITS = 128;
 
+    /**
+     * Stores an IPv6 netmask
+     */
     private int[] netmask = new int[]{0,0,0,0};
+    /**
+     * Stores an IPv6 address
+     */
     private int[] address = new int[]{0,0,0,0};
+    /**
+     * Stores an IPv6 network address
+     */
     private int[] network = new int[]{0,0,0,0};
+    /**
+     * Stores an IPv6 broadcast address
+     */
     private int[] broadcast = new int[]{0,0,0,0};
+    /**
+     * Stores the number of bits used for the net part
+     */
     private int cidr = 0;
 
-    /** Whether the broadcast/network address are included in host count */
+    /**
+     * Whether the broadcast/network address are included in host count
+     */
     private boolean inclusiveHostCount = false;
 
-
+    /**
+     * Creates a SubnetUtilsV6 object from a subnet as String
+     * @param cidrNotation subnet
+     * @return
+     */
     public static SubnetUtilsV6 createSubnet(String cidrNotation){
         SubnetUtils oldUtils = null;
         boolean oldUtilsError = false;
@@ -38,6 +66,12 @@ public class SubnetUtilsV6 {
             return new SubnetUtilsV6(oldUtils);
     }
 
+    /**
+     * Creates a SubnetUtilsV6 object from a address and a netmask as Strings
+     * @param address
+     * @param mask
+     * @return
+     */
     public static SubnetUtilsV6 createSubnet(String address, String mask){
         SubnetUtils oldUtils = null;
         boolean oldUtilsError = false;
@@ -52,23 +86,42 @@ public class SubnetUtilsV6 {
             return new SubnetUtilsV6(oldUtils);
     }
 
-
+    /**
+     * Constructor for an IPv4 subnet using the SubnetUtils object
+     * @param oldUtils
+     */
     protected SubnetUtilsV6(SubnetUtils oldUtils){
         this.oldUtils = oldUtils;
     }
 
+    /**
+     * Constructor for an IPv6 subnet as String
+     * @param cidrNotation
+     */
     protected SubnetUtilsV6(String cidrNotation){
         calculate(cidrNotation);
     }
 
+    /**
+     * Constructor for an IPv6 subnet as address and netmask as String
+     * @param address
+     * @param mask
+     */
     protected SubnetUtilsV6(String address, String mask){
         calculate(toCidrNotation(address, mask));
     }
 
+    /**
+     * If an IPv4 address is used with a SubnetUtils object
+     * @return
+     */
     public boolean isV6(){
         return oldUtils==null;
     }
 
+    /**
+     * Utility class to compute address and masks for the IPv6 subnet
+     */
     public final class SubnetInfoV6 {
 
         /* Mask to convert unsigned int to a long (i.e. keep 32 bits) */
@@ -79,33 +132,86 @@ public class SubnetUtilsV6 {
                 oldInfo = oldUtils.getInfo();
         }
 
+        /**
+         * SubnetInfo for IPv4 subnet
+         */
         private SubnetUtils.SubnetInfo oldInfo;
 
+        /**
+         * Returns the subnet's netmask
+         * @return
+         */
         private int[] netmask()       { return netmask; }
+
+        /**
+         * Returns the subnet's network address
+         * @return
+         */
         private int[] network()       { return network; }
+
+        /**
+         * Returns the IPv6 address
+         * @return
+         */
         private int[] address()       { return address; }
+
+        /**
+         * Returns the subnet's broadcast address
+         * @return
+         */
         private int[] broadcast()     { return broadcast; }
 
+        /**
+         * If SubnetInfo for an IPv4 address is used
+         * @return
+         */
         public boolean isV6(){
             return oldInfo==null;
         }
 
         // long versions of the values (as unsigned int) which are more suitable for range checking
+
+        /**
+         * Subnet's network address as long
+         * @return
+         */
         private long[] networkLong()  {
             long[] ret = new long[4];
             for(int i=0; i<4; i++)
                 ret[i] = network[i] &  UNSIGNED_INT_MASK;
             return ret;
         }
+
+        /**
+         * Return the network address from an IPv6 address in this subnet.
+         * @param x IPv6 address
+         * @return
+         */
         private long networkLong(int x)  { return network[x] &  UNSIGNED_INT_MASK; }
+
+        /**
+         * Subnet's broadcast address as long
+         * @return
+         */
         private long[] broadcastLong() {
             long[] ret = new long[4];
             for(int i=0; i<4; i++)
                 ret[i] = broadcast[i] &  UNSIGNED_INT_MASK;
             return ret;
         }
+
+        /**
+         * Return the broadcast address from an IPv6 address in this subnet.
+         * @param x IPv6 address
+         * @return
+         */
         private long broadcastLong(int x){ return broadcast[x] &  UNSIGNED_INT_MASK; }
 
+        /**
+         * Converts an IPv6 address from int[] to long[] representation
+         * @param arr IPv6 address
+         * @return
+         */
         private long[] toLongArray(int[] arr){
             long[] ret = new long[4];
             for(int i=0; i<4; i++)
@@ -113,6 +219,10 @@ public class SubnetUtilsV6 {
             return ret;
         }
 
+        /**
+         * Returns the lowest IPv6 address in this subnet
+         * @return
+         */
         private int[] low() {
             if(isInclusiveHostCount())
                 return network();
@@ -131,6 +241,10 @@ public class SubnetUtilsV6 {
             }
         }
 
+        /**
+         * Returns the highest IPv6 address in this subnet
+         * @return
+         */
         private int[] high() {
             if(isInclusiveHostCount())
                 return broadcast();
@@ -149,6 +263,11 @@ public class SubnetUtilsV6 {
             }
         }
 
+        /**
+         * Checks if the given IPv6 belongs to this subnet.
+         * @param address IPv6 address
+         * @return
+         */
         public boolean isInRange(String address) {
             if(oldInfo==null)
                 return isInRange(toInteger(address));
@@ -156,6 +275,11 @@ public class SubnetUtilsV6 {
                 return oldInfo.isInRange(address);
         }
 
+        /**
+         * Checks if the given IPv6 belongs to this subnet.
+         * @param address IPv6 address
+         * @return
+         */
         public boolean isInRange(int address){
             if(oldInfo==null)
                 throw new IllegalArgumentException();
@@ -163,6 +287,11 @@ public class SubnetUtilsV6 {
                 return oldInfo.isInRange(address);
         }
 
+        /**
+         * Checks if the given IPv6 belongs to this subnet.
+         * @param address IPv6 address
+         * @return
+         */
         public boolean isInRange(int[] address) {
             long[] add = toLongArray(address);
             long[] low = toLongArray(low());
@@ -174,6 +303,10 @@ public class SubnetUtilsV6 {
             return true;
         }
 
+        /**
+         * Returns this subnet's broadcast address as String
+         * @return
+         */
         public String getBroadcastAddress() {
             if(oldInfo==null)
                 return format(toArray(broadcast()));
@@ -181,6 +314,10 @@ public class SubnetUtilsV6 {
                 return oldInfo.getBroadcastAddress();
         }
 
+        /**
+         * Returns this subnet's network address as String
+         * @return
+         */
         public String getNetworkAddress() {
             if(oldInfo==null)
                 return format(toArray(network()));
@@ -188,6 +325,10 @@ public class SubnetUtilsV6 {
                 return oldInfo.getNetworkAddress();
         }
 
+        /**
+         * Returns this subnet's netmask as String
+         * @return
+         */
         public String getNetmask() {
             if(oldInfo==null)
                 return format(toArray(netmask()));
@@ -195,6 +336,10 @@ public class SubnetUtilsV6 {
                 return oldInfo.getNetmask();
         }
 
+        /**
+         * Returns the IPv6 this subnet is based on as String
+         * @return
+         */
         public String getAddress() {
             if(oldInfo==null)
                 return format(toArray(address()));
@@ -202,6 +347,10 @@ public class SubnetUtilsV6 {
                 return oldInfo.getAddress();
         }
 
+        /**
+         * Return the number of bits in the netmask
+         * @return
+         */
         public int getCidrBits(){
             if(oldInfo!=null) {
                 return Integer.parseInt(oldInfo.getCidrSignature().split("/")[1]);
@@ -209,6 +358,10 @@ public class SubnetUtilsV6 {
             return cidr;
         }
 
+        /**
+         * Returns the lowest address of this subnet as String
+         * @return
+         */
         public String getLowAddress() {
             if(oldInfo==null)
                 return format(toArray(low()));
@@ -216,6 +369,10 @@ public class SubnetUtilsV6 {
                 return oldInfo.getLowAddress();
         }
 
+        /**
+         * Returns the highest address of this subnet as String
+         * @return
+         */
         public String getHighAddress() {
             if(oldInfo==null)
                 return format(toArray(high()));
@@ -223,18 +380,31 @@ public class SubnetUtilsV6 {
                 return oldInfo.getHighAddress();
         }
 
+        /**
+         * Returns the number of IP addresses in this subnet
+         * @return
+         */
         public long getAddressCountLong() {
             if(oldInfo!=null)
                 return oldInfo.getAddressCountLong();
             return getAddressCountLong(64);
         }
 
+        /**
+         * Returns the number of sub subnets with a netmask of @subnetSize bits
+         * @param subnetSize Number of bits the sub subnets' netmasks have
+         * @return
+         */
         public long getAddressCountLong(int subnetSize) {
             int netmaskcidr = pop(netmask());
             long count = (long)Math.pow(2, subnetSize-netmaskcidr);
             return count;
         }
 
+        /**
+         * Returns the Cidr Signature as String
+         * @return
+         */
         public String getCidrSignature() {
             if(oldInfo!=null)
                 oldInfo.getCidrSignature();
@@ -244,6 +414,10 @@ public class SubnetUtilsV6 {
             );
         }
 
+        /**
+         * Returns all IP addresses as String array
+         * @return
+         */
         public String[] getAllAddresses(){
             if(oldInfo!=null)
                 return oldInfo.getAllAddresses();
@@ -251,6 +425,11 @@ public class SubnetUtilsV6 {
                 return getAllAddresses(64);
         }
 
+        /**
+         * Returns all sub subnets with a fixed netmask as String array
+         * @param subnetSize Sub subnets' netmask bit length
+         * @return
+         */
         public String[] getAllAddresses(int subnetSize) {
             int ct = (int) getAddressCountLong(subnetSize);
             if(ct <= 0)
@@ -292,10 +471,10 @@ public class SubnetUtilsV6 {
             return addresses;
         }
 
-
-
-
-
+        /**
+         * Returns information about the subnet
+         * @return
+         */
         @Override
         public String toString() {
             if(oldInfo!=null)
@@ -313,19 +492,32 @@ public class SubnetUtilsV6 {
 
     }
 
+    /**
+     * Whether the broadcast/network address are included in host count
+     * @return
+     */
     public boolean isInclusiveHostCount() {
         return inclusiveHostCount;
     }
 
+    /**
+     * Sets whether the broadcast/network address are included in host count
+     * @param inclusiveHostCount
+     */
     public void setInclusiveHostCount(boolean inclusiveHostCount) {
         this.inclusiveHostCount = inclusiveHostCount;
     }
 
+    /**
+     * Returns the underlying SubnetInfoV6 object
+     * @return
+     */
     public SubnetInfoV6 getInfo() { return new SubnetInfoV6(); }
 
 
-    /*
+    /**
     * Initialize the internal fields from the supplied CIDR mask
+    * @param mask CIDR mask
     */
     private void calculate(String mask) {
 
@@ -359,7 +551,7 @@ public class SubnetUtilsV6 {
         }
     }
 
-    /*
+    /**
     * Convert a packed integer address into a 8-element array
     */
     private int[] toArray(int[] val) {
@@ -372,6 +564,11 @@ public class SubnetUtilsV6 {
         return ret;
     }
 
+    /**
+     * Convert a the address from long[] to int[] representation
+     * @param val
+     * @return
+     */
     private int[] toArray(long[] val) {
         int ret[] = new int[8];
 
@@ -414,11 +611,16 @@ public class SubnetUtilsV6 {
 
     }
 
+    /**
+     * Counts the number of 1-bits in the int[] array
+     * @param x
+     * @return
+     */
     protected int pop(int[] x){
         return pop(x[0]) + pop(x[1]) + pop(x[2]) + pop(x[3]);
     }
 
-    /*
+    /**
     * Count the number of 1-bits in a 32-bit integer using a divide-and-conquer strategy
     * see Hacker's Delight section 5.1
     */
@@ -431,8 +633,13 @@ public class SubnetUtilsV6 {
         return x & 0x0000003F;
     }
 
+    /**
+     * Converts an address and netmask as String to the CIDR notation
+     * @param addr
+     * @param mask
+     * @return
+     */
     protected String toCidrNotation(String addr, String mask) {
         return addr + "/" + pop(toInteger(mask));
     }
-
 }

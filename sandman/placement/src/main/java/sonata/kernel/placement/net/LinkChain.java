1acb350550a67ba004a34408b457e17369023ec5
Alex <foo@example.org>
2017-02-27 17:32:30 +0100

    Add source code comments to parts of the net package

--- sandman/placement/src/main/java/sonata/kernel/placement/net/LinkChain.java
+++ sandman/placement/src/main/java/sonata/kernel/placement/net/LinkChain.java
@@ -1,27 +1,58 @@
 package sonata.kernel.placement.net;
 
 import sonata.kernel.placement.config.PopResource;
-
-import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Contains information about the network chain between two datacenter nodes.
+ */
 public class LinkChain {
 
+    /**
+     * The chain's source port
+     */
     public LinkPort srcPort;
+    /**
+     * The chain's destination port
+     */
     public LinkPort dstPort;
+    /**
+     * Optional list of switches used for the chain
+     */
     public List<String> path = null;
 
+    /**
+     * Creates a new chain descriptor
+     * @param srcDc source datacenter
+     * @param srcStack source stack
+     * @param srcServer source server name
+     * @param srcPort source server's port
+     * @param dstDc destination datacenter
+     * @param dstStack destination stack
+     * @param dstServer destination server
+     * @param dstPort destination server's port
+     */
     public LinkChain(PopResource srcDc, String srcStack, String srcServer, String srcPort,
                      PopResource dstDc, String dstStack, String dstServer, String dstPort) {
         this.srcPort = new LinkPort(srcDc, srcStack, srcServer, srcPort);
         this.dstPort = new LinkPort(dstDc, dstStack, dstServer, dstPort);
     }
 
+    /**
+     * Creates a new chain descriptor
+     * @param srcPort source port
+     * @param dstPort destination port
+     */
     public LinkChain(LinkPort srcPort, LinkPort dstPort){
         this.srcPort = srcPort;
         this.dstPort = dstPort;
     }
 
+    /**
+     * Compares this chain to another chain descriptor
+     * @param obj another chain descriptor
+     * @return true if the given chain describes the same chain, else false
+     */
     public boolean equals(Object obj){
         if(obj == null || !(obj instanceof LinkChain))
             return false;
--- sandman/placement/src/main/java/sonata/kernel/placement/net/LinkLoadbalance.java
+++ sandman/placement/src/main/java/sonata/kernel/placement/net/LinkLoadbalance.java
@@ -1,21 +1,41 @@
 package sonata.kernel.placement.net;
 
 import sonata.kernel.placement.config.PopResource;
-
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Contains information about a network loadbalance rule
+ */
 public class LinkLoadbalance {
 
+    /**
+     * Source port
+     */
     public LinkPort srcPort;
-
+    /**
+     * List of destination ports that should be loadbalanced
+     */
     public List<LinkPort> dstPorts = new ArrayList<LinkPort>();
 
+    /**
+     * Creates a new loadbalance rule
+     * @param srcPort source port
+     * @param dstPorts list of destination ports
+     */
     public LinkLoadbalance(LinkPort srcPort, List<LinkPort> dstPorts){
         this.srcPort = srcPort;
         this.dstPorts.addAll(dstPorts);
     }
 
+    /**
+     * Creates a new loadbalance rule
+     * @param srcDc source datacenter
+     * @param srcStack source stack
+     * @param srcServer source server
+     * @param srcPort source server's port
+     * @param dstPorts list of destination ports
+     */
     public LinkLoadbalance(PopResource srcDc, String srcStack, String srcServer, String srcPort,
                            List<LinkPort> dstPorts){
         this.srcPort = new LinkPort(srcDc, srcStack, srcServer, srcPort);
